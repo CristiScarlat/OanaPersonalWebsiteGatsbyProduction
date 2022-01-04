1a638299a6380fc9da1dc8bb87ded01ae2c7ed6b
CristiScarlat <78.cristi@gmail.com>
2022-01-04 21:45:29 +0200

    fix home page background, fix background change by route pathname, stryling

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,29 +11,7 @@ const IndexPage = () => {
 
   return (
     <Layout>
-      <Container fluid className="home-container mt-3">
-        <Row className="justify-content-md-start">
-          <Col md="auto">
-            <div style={{ position: 'relative' }}>
-              <img src="img/home-no-text.jpg" style={{ width: '100%' }} />
-              <p id="paul-gauguin-min-420" style={{ position: 'absolute', top: '50%', right: 0 }}>{"\"I shot my eyes in order to see.\" ~Paul Gauguin"}</p>
-            </div>
-          </Col>
-        </Row>
-        <Row id="oana-name" className="justify-content-md-center mt-2 mb-2">
-          <Col md="auto">
-            <p id="paul-gauguin-max-420">{"\"I shot my eyes in order to see.\" ~Paul Gauguin"}</p>
-            <p>PhD Artist Oana Bolog-Bleich</p>
-          </Col>
-        </Row>
-        <Row>
-          <div className="home-images-container">
-            {(allImages && allImages.length > 0) && allImages.filter(i => i.aspectRatio === 1).map((imgObj, index) => <Link key={imgObj.src + index} to="/portofolio">
-              <img width="320px" height={320 / imgObj.aspectRatio} key={imgObj.src} src={imgObj.src} className="m-1" />
-            </Link>)}
-          </div>
-        </Row>
-      </Container>
+        {/* <Link to="/portofolio/" className="home-button">Welcom to my world</Link> */}
     </Layout>
   )
 }
