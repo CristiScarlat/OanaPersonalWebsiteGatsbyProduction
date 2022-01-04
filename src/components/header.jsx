2cd477b64fe405e56e42ba556118e59c0b9bff37
Cristi Scarlat <78.cristi@gmail.com>
2022-01-04 21:49:37 +0200

    Develop (#2)
    * fix favicon, new build
    
    * fix home page background, fix background change by route pathname, stryling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,14 +8,14 @@ const Header = () => {
     }
     return (
         <Navbar bg="dark" variant="dark" expand="lg" className="justify-content-between" style={{fontSize: 'larger'}}>
-            <Navbar.Brand></Navbar.Brand>
+            <Navbar.Brand style={{fontSize: '14px'}}>Phd artist Oana Bolog-Bleich</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav" style={{flexGrow: 0}}>
                 <Nav className="mr-auto">
                     <Link to="/" style={{marginRight: '1rem', color: 'grey'}} getProps={getPropsForLink}>Home</Link>
-                    <Link to="/portofolio/" style={{marginRight: '1rem', color: 'grey'}} getProps={getPropsForLink}>Paintings</Link>
+                    <Link to="/portofolio" style={{marginRight: '1rem', color: 'grey'}} getProps={getPropsForLink}>Paintings</Link>
                     <Link to="/media" style={{marginRight: '1rem', color: 'grey'}} activeStyle={{color: 'white', fontWeight: 'bold'}}>Interviews and media</Link>
-                    <Link to="/about/" style={{marginRight: '1rem', color: 'grey'}} getProps={getPropsForLink}>About me</Link>
+                    <Link to="/about" style={{marginRight: '1rem', color: 'grey'}} getProps={getPropsForLink}>About me</Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
