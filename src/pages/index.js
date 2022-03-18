38a37b69ffa7ffe43fd411a1fe0852ef3595203d
Cristi Scarlat <78.cristi@gmail.com>
2022-03-18 22:57:05 +0200

    Develop (#6)
    * fix favicon, new build
    
    * fix home page background, fix background change by route pathname, stryling
    
    * remove shared button from the galery
    
    * add react-helmet plugin for seo and clean unused code
    
    * add button for large text description, renamed pictures to have index and title, delete the gif files
    
    * add description for every album, css for same sized pictures
    
    * fix style for tabs
    
    * fix ui-large-text page error
    
    * fix link in header and remove unused code
    
    * upgrade gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,13 @@
 import * as React from "react"
 import Layout from "../components/layout";
-import { Container, Row, Col } from "react-bootstrap";
-//import { UI_TEXT } from './constants';
-import GraphqlContext from "../context/graphDataContext";
 import { Link } from 'gatsby'
 import './css/index.css'
 
 const IndexPage = () => {
-  const { allImages } = React.useContext(GraphqlContext)
 
   return (
     <Layout>
-        <Link to="/portofolio/" className="home-button">Wellcome</Link>
+        <Link to="/portofolio" className="home-button">Wellcome</Link>
     </Layout>
   )
 }
